6273cb160287ea78fe4c6fbea02e22625acaef88
Ankita Akshay Chougule <ankitanavalgunde161999@gmail.com>
2024-11-24 22:35:39 +0530

    Update 4. Rotate an Array by d - Counterclockwise or Left.py

--- 4. Rotate an Array by d - Counterclockwise or Left.py
+++ 4. Rotate an Array by d - Counterclockwise or Left.py
@@ -1,36 +1,58 @@
-# Python Code to left rotate an array using Reversal Algorithm
+#User function Template for python3
 
-# Function to rotate an array by d elements to the left
-def rotateArr(arr, d):
-    n = len(arr)
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self, arr, d):
+        #Your code here
+        n = len(arr)
+        d = d%n # Handle cases where d > n
+        
+        # Call the reverse function
+        self.reverse(arr, 0, d-1)
+        self.reverse(arr, d, n-1)
+        self.reverse(arr, 0, n-1)
+        
+    # Function to reverse a segment of the array    
+    def reverse(self, arr, s, e):
+        while s < e:
+            arr[s],arr[e] = arr[e],arr[s]
+            s = s+1
+            e = e-1
+            
+    
 
-    # Handle the case where d > size of array
-    d %= n
 
-    # Reverse the first d elements
-    reverse(arr, 0, d - 1)
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
 
-    # Reverse the remaining n-d elements
-    reverse(arr, d, n - 1)
+import math
 
-    # Reverse the entire array
-    reverse(arr, 0, n - 1)
 
-# Function to reverse a portion of the array
-def reverse(arr, start, end):
-    while start < end:
-        arr[start], arr[end] = arr[end], arr[start]
-        start += 1
-        end -= 1
+def main():
+    T = int(input())
+
+    while (T > 0):
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ob.rotateArr(A, D)
+
+        for i in A:
+            print(i, end=" ")
+
+        print()
+
+        T -= 1
+
+        print("~")
+
 
 if __name__ == "__main__":
-    arr = [1, 2, 3, 4, 5, 6]
-    d = 2
-    
-    rotateArr(arr, d)
-  
-    for i in range(len(arr)):
-        print(arr[i], end=" ")
+    main()
+
+# } Driver Code Ends
 
 
 """
