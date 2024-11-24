d1e2dbda32004a2c9b67ac061b2785b17c098da1
Ankita Akshay Chougule <ankitanavalgunde161999@gmail.com>
2024-11-24 14:18:15 +0530

    Update Array Reverse.py

--- Array Reverse.py
+++ Array Reverse.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
 
 """
-Step	i	    arr[i]	arr[n - i - 1]	    Swap Result
+Step	    i	    arr[i]	arr[n - i - 1]	    Swap Result
 1	    0	    1	        5	            [5, 4, 3, 2, 6, 1]
 2	    1	    4	        6	            [5, 6, 3, 2, 4, 1]
 3	    2	    3	        2	            [5, 6, 2, 3, 4, 1]
